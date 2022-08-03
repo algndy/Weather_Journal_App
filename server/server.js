08356f5ec83b36ff533fd9c8b83e3196b4635be8
Mohamed Kamal Abdelaziz Ahmed <mohamedkamal.info@gmail.com>
2022-08-03 13:39:56 +0200

    modified dir name

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 app.use(cors());
 
-app.use(express.static(path.join(__dirname,'..','webPage')));
+app.use(express.static(path.join(__dirname,'..','docs')));
 
 const server = app.listen(port,()=>{
     console.log('Server is Running...');
